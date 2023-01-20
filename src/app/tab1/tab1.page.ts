c4bd3fe5745d309e9d9a63432b8f7884c11a04c5
samanthaeunic20 <huapillasamanthaa@gmail.com>
2023-01-19 19:32:04 -0600

    respaldo app comandos GIT

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page  implements OnInit {
+
+  constructor() {}
+
+  ngOnInit():void{
+  console.log("Hola mundo zx");
+
+  let numero=20;
+  debugger
+  numero=10;
+  numero=15;
+  numero=100;
+  console.log("La variable esta tomando este valor"+numero);
+
+}
+}
